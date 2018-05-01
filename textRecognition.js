3c84561c820489fce391b10e0db860eb2bb0af46
lndvieira <lean_vieiras@hotmail.com>
2018-05-01 18:24:53 -0300

    adicao da funcao de reconhecimento

---
+++ textRecognition.js
@@ -0,0 +1,97 @@
+'use strict';
+
+let https = require ('https');
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace the accessKey string value with your valid access key.
+let accessKey = '8c46514faebd4dffbb6d242f3cca794a';
+
+// Replace or verify the region.
+
+// You must use the same region in your REST API call as you used to obtain your access keys.
+// For example, if you obtained your access keys from the westus region, replace 
+// "westcentralus" in the URI below with "westus".
+
+// NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
+// a free trial access key, you should not need to change this region.
+let uri = 'brazilsouth.api.cognitive.microsoft.com';
+let pathLanguages = '/text/analytics/v2.0/languages';
+let pathSentiment = '/text/analytics/v2.0/sentiment';
+let pathKeyPhrases = '/text/analytics/v2.0/keyPhrases';
+
+let response_handler = function (response) {
+    let body = '';
+    response.on ('data', function (d) {
+        body += d;
+    });
+    response.on ('end', function () {
+        let body_ = JSON.parse (body);
+        let body__ = JSON.stringify (body_, null, '  ');
+        console.log (body__);
+    });
+    response.on ('error', function (e) {
+        console.log ('Error: ' + e.message);
+    });
+};
+
+let get_language = function (documents) {
+    let body = JSON.stringify (documents);
+
+    let request_params = {
+        method : 'POST',
+        hostname : uri,
+        path : pathLanguages,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : accessKey,
+        }
+    };
+
+    let req = https.request (request_params, response_handler);
+    req.write (body);
+    req.end ();
+}
+
+let get_sentiments = function (documents) {
+    let body = JSON.stringify (documents);
+
+    let request_params = {
+        method : 'POST',
+        hostname : uri,
+        path : pathSentiment,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : accessKey,
+        }
+    };
+
+    let req = https.request (request_params, response_handler);
+    req.write (body);
+    req.end ();
+}
+
+let get_key_phrases = function (documents) {
+    let body = JSON.stringify (documents);
+
+    let request_params = {
+        method : 'POST',
+        hostname : uri,
+        path : pathKeyPhrases,
+        headers : {
+            'Ocp-Apim-Subscription-Key' : accessKey,
+        }
+    };
+
+    let req = https.request (request_params, response_handler);
+    req.write (body);
+    req.end ();
+}
+
+let documents = { 'documents': [
+    { 'id': '1', 'text': 'Não gostei do café, a comida era horrivel' }
+]};
+
+get_language (documents);
+get_sentiments (documents);
+get_key_phrases (documents);
